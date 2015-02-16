433ce80275cd6ed4ddf3a687ff005bcc35f8a946
Alon Dahari <climbinghobo@gmail.com>
2015-02-16 15:37:51 -0700

    add text file response

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var fs = require('fs');
 
+var fileContents = fs.readFileSync(__dirname + '/public/data.txt');
+
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-	res.render('index');
+	res.header('Content-Type', 'text/html');
+	res.send(fileContents);
+	// res.render('index');
 });
 
 var server = app.listen(4541, function() {
