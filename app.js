5b5173271fd7a62f72dc5df8abe0428a54f0164e
Alon Dahari <climbinghobo@gmail.com>
2015-02-16 15:43:34 -0700

    made request asynchronous

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var fs = require('fs');
 
-var fileContents = fs.readFileSync(__dirname + '/public/data.txt');
 
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-	res.header('Content-Type', 'text/html');
-	res.send(fileContents);
-	// res.render('index');
+var fileContents = fs.readFile(__dirname + '/public/data.txt', function(err, data){
+	app.get('/', function(req, res) {
+		res.header('Content-Type', 'text/html');
+		res.send(data);
+	});
 });
 
 var server = app.listen(4541, function() {
